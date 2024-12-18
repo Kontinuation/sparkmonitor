4409ebf1f0a9247534814ac6bee659009dda4823
Kristin Cowalcijk <bo@wherobots.com>
2024-12-18 15:50:29 +0800

    Move the sparkmonitor widget into the output area

--- src/lab-extension/jupyterlab-sparkmonitor.ts
+++ src/lab-extension/jupyterlab-sparkmonitor.ts
@@ -6,10 +6,10 @@ import {
   IKernelConnection
 } from '@jupyterlab/services/lib/kernel/kernel';
 import { ICommMsgMsg } from '@jupyterlab/services/lib/kernel/messages';
-import { PanelLayout } from '@lumino/widgets';
 import CurrentCellTracker from './current-cell';
 import { CellWidget } from '../components';
 import { ReactWidget } from '@jupyterlab/apputils';
+import { CodeCell } from '@jupyterlab/cells';
 
 import type { NotebookStore } from '../store/notebook';
 export default class JupyterLabSparkMonitor {
@@ -52,18 +52,23 @@ export default class JupyterLabSparkMonitor {
     });
   }
 
-  /** Remember which cells have the spark monitor widget created. */
-  cellWidgetMap: { [cellId: string]: boolean } = {};
-
   createElementIfNotExists(cellModel: ICellModel) {
-    if (this.cellWidgetMap[cellModel.id] === true) {
-      return;
-    }
     if (cellModel.type === 'code') {
       const codeCell = this.notebookPanel.content.widgets.find(
         widget => widget.model === cellModel
-      );
+      ) as CodeCell;
+
       if (codeCell && !codeCell.node.querySelector('.sparkMonitorCellRoot')) {
+        // Get the output area directly from the code cell
+        const outputArea = codeCell.outputArea;
+        if (!outputArea) {
+          return;
+        }
+        if (outputArea.layout.widgets.find(widget => widget.hasClass('spark-monitor-cell-widget'))) {
+          return;
+        }
+
+        // Insert the widget at the beginning of the output area
         const widget = ReactWidget.create(
           React.createElement(CellWidget, {
             notebookId: this.notebookPanel.id,
@@ -71,18 +76,8 @@ export default class JupyterLabSparkMonitor {
           })
         );
         widget.addClass('spark-monitor-cell-widget');
-
-        // Insert the widget above the output wrapper based on position/index.
-        // If there is no output wrapper found, insert it using the default index.
-        const outputWrapperIndex = (codeCell.layout as PanelLayout).widgets.findIndex(
-          widget => widget.hasClass('jp-Cell-outputWrapper'));
-        const insertIndex = outputWrapperIndex > 0? outputWrapperIndex - 1: 2;
-        (codeCell.layout as PanelLayout).insertWidget(
-          insertIndex,
-          widget
-        );
+        outputArea.layout.insertWidget(0, widget);
         codeCell.update();
-        this.cellWidgetMap[cellModel.id] = true;
       }
     }
   }
@@ -119,7 +114,6 @@ export default class JupyterLabSparkMonitor {
       console.warn('SparkMonitor: Job started with no running cell.');
       return;
     }
-    this.createElementIfNotExists(cell.model);
     // See if we have a new execution. If it's new (a cell has been run again) we need to clear the cell monitor
     const newExecution =
       this.currentCellTracker.getNumCellsExecuted() >
@@ -130,6 +124,7 @@ export default class JupyterLabSparkMonitor {
       this.notebookStore.onCellExecutedAgain(cell.model.id);
     }
     this.notebookStore.onSparkJobStart(cell.model.id, data);
+    this.createElementIfNotExists(cell.model);
   }
 
   onSparkStageSubmitted(data: any) {
@@ -138,8 +133,8 @@ export default class JupyterLabSparkMonitor {
       console.warn('SparkMonitor: Stage started with no running cell.');
       return;
     }
-    this.createElementIfNotExists(cell.model);
     this.notebookStore.onSparkStageSubmitted(cell.model.id, data);
+    this.createElementIfNotExists(cell.model);
   }
 
   handleMessage(msg: ICommMsgMsg) {
